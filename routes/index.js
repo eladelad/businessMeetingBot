280bf196693a194c9de11046f6c3b85a7da42250
elad <eladh87@gmail.com>
2017-02-25 16:31:02 +0200

    fixed conf

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var request = require('request');
-var conf = require('./conf').conf;
+var conf = require('../conf').conf;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
